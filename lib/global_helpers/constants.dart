0ec875218ce776d9785d40a5ce23d8a75dc97184
Khaled Khalifa <96270919+khaledkhalifa74@users.noreply.github.com>
2024-01-09 19:15:09 +0200

    colors and fonts

---
+++ lib/global_helpers/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//theme colors
+const kPrimaryColor = Color(0xFF1DBF73);
+const kSecondaryColor = Color(0xFF696F79);
+const kWhiteColor = Colors.white;
+const kBlackColor = Colors.black;
+const kHintColor = Color(0xFFC3C5C8);
+const kFieldColor = Color(0xFFF9F9F9);
+const kProgressLineColor = Color(0xFFE6EAEF);
+const kErrorColor = Color(0xFFF56242);
+const kErrorBodyColor = Color(0xFFFFF0ED);
+const kSkillBodyColor = Color(0xFFE9F9F1);
+const kBorderColor = Color(0xFFE6EAEF);
+
+//icon colors
+const kPrimaryIconColor = Color(0xFF8692A6);
+const kSecondaryIconColor = Color(0xFFC3C5C8);
+
